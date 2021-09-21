33d898b0563bdaa9362f5baf1f83d60baa9945b7
Deepakpanghal <91010930+Deepakpanghal@users.noreply.github.com>
2021-09-21 16:27:13 +0530

    Add files via upload

---
+++ TypeCast.java
@@ -0,0 +1,22 @@
+package com.geekster.homework;
+
+public class TypeCast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 45;
+		float b = a;
+		double z = b;
+		
+		System.out.println("This is Integer Value of a" +"  " + a);
+		
+		System.out.println("This is Value of a after typecast into Float" +"  " + b);
+		
+		System.out.println("This is Double Value of b" +"  " + z);
+		
+		
+	}
+
+
+}
