1cdeb58c54ff213685ad3d1aa9461fb35b3f07e5
Deepakpanghal <91010930+Deepakpanghal@users.noreply.github.com>
2021-09-21 16:26:05 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,19 @@
+package com.geekster.homework;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		B objB =new B();
+		objB.add();
+
+	
+	//	B.add(); for static method
+		
+		
+
+					
+	}
+
+}
